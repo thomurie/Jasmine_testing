21da3333eca3ee079c5c409c233ec3b8fce70307
Thomas Urie <thomasurie@Thomass-MacBook-Pro.local>
2021-04-09 21:50:01 -0600

    changes made 04/09

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,25 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
+    serverNameInput.value = "Alice";
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
+  it("should add a new server to allServers on submitServerInfo()", function () {
     submitServerInfo();
 
     expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
   });
 
-  afterEach(function() {
-    // teardown logic
+  it("should update the Servertable on updateServerTable()", function () {
+    submitServerInfo();
+    updateServerTable();
+
+    expect(Boolean(document.getElementById("server" + serverId))).toBe(true);
+  });
+
+  afterEach(function () {
+    serverId = 0;
+    serverTbody.innerHTML = "";
+    allServers = {};
   });
 });
